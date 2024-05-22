3a1a1df8959e044c836ba4ec70f0ad96597a1ff6
chiomaowuama <108200300+chiomaowuama@users.noreply.github.com>
2024-05-22 15:12:14 +0100

    done the email

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ function verify(event) {
   event.preventDefault();
   let Fname = document.getElementById("fname").value;
   let Sname = document.getElementById("sname").value;
+  //   let Email = document.getElementById("email").value;
   let Errormgs = document.getElementById("errormgs");
   let Errormg = document.getElementById("errormg");
+  //   let Emailerror = document.getElementById("emailerror");
   // firstname verification
   const regex = /^[A-Za-z]+$/;
   if (Fname === "" || Fname == null) {
@@ -13,7 +15,7 @@ function verify(event) {
     if (!regex.test(Fname)) {
       Errormgs.innerHTML = "please Alphabets only";
     } else {
-      console.log("yes");
+      Errormgs.innerHTML = "";
     }
   }
   // surnname verification
@@ -24,10 +26,19 @@ function verify(event) {
     if (!regex.test(Sname)) {
       Errormg.innerHTML = "please Alphabets only";
     } else {
-      console.log("yes");
+      Errormg.innerHTML = "";
     }
   }
   //   Email verification
-  //   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  //   return emailRegex.test(email);
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  console.log(Email);
+  if (Email === " " || Email == null) {
+    Emailerror.innerHTML = "please input Surnname";
+  } else {
+    if (!emailRegex.test(Email)) {
+      Emailerror.innerHTML = "Email incorrect";
+    } else {
+      Emailerror.innerHTML = " ";
+    }
+  }
 }
