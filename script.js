4c94dac5e9228953b0d1568554bb7bac7ebd4728
chiomaowuama <108200300+chiomaowuama@users.noreply.github.com>
2024-05-22 13:35:14 +0100

    doing error message

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+function verify(event) {
+  event.preventDefault();
+  let Fname = document.getElementById("fname").value;
+  let Snamme = document.getElementById("sname").value;
 
+  const regex = /^[A-Za-z]+$/;
+  if (Fname === " " || Fname == null) {
+  }
+  if (!regex.test(Fname)) {
+    console.log(" no");
+  } else {
+    console.log("yes");
+  }
+  //   console.log(Fnamme, Snamme);
+}
