e0fbcad933d0509328500f0ba080b22d78121c06
chiomaowuama <108200300+chiomaowuama@users.noreply.github.com>
2024-05-22 14:35:44 +0100

    done with the names

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
 function verify(event) {
   event.preventDefault();
   let Fname = document.getElementById("fname").value;
-  let Snamme = document.getElementById("sname").value;
-
+  let Sname = document.getElementById("sname").value;
+  let Errormgs = document.getElementById("errormgs");
+  let Errormg = document.getElementById("errormg");
+  // firstname verification
   const regex = /^[A-Za-z]+$/;
-  if (Fname === " " || Fname == null) {
-  }
-  if (!regex.test(Fname)) {
-    console.log(" no");
+  if (Fname === "" || Fname == null) {
+    console.log("nothing");
+    Errormgs.innerHTML = "please input firstname";
   } else {
-    console.log("yes");
+    if (!regex.test(Fname)) {
+      Errormgs.innerHTML = "please Alphabets only";
+    } else {
+      console.log("yes");
+    }
   }
-  //   console.log(Fnamme, Snamme);
+  // surnname verification
+  if (Sname === "" || Sname == null) {
+    console.log("nothing");
+    Errormg.innerHTML = "please input Surnname";
+  } else {
+    if (!regex.test(Sname)) {
+      Errormg.innerHTML = "please Alphabets only";
+    } else {
+      console.log("yes");
+    }
+  }
+  //   Email verification
 }
